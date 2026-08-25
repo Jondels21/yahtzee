72f0eff92085621252c9ab0f464971ad89f9bc4b
Janne Salovaara <jannesalovaara@gmail.com>
2026-08-25 14:48:29 +0300

    get server url from .env or default to localhost

--- frontend/src/socket/socket.ts
+++ frontend/src/socket/socket.ts
@@ -1,6 +1,6 @@
-import { io } from "socket.io-client"
+import { io } from "socket.io-client";
 
-const SERVER_URL = "http://localhost:3000";
+const SERVER_URL = import.meta.env.VITE_API_URL ?? "http://localhost:3000";
 
 export const socket = io(SERVER_URL, {
   autoConnect: true,
